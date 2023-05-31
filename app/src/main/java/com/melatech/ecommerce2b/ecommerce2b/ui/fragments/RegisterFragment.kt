646444c59117efa3bff827fd8e136927187ec79e
melatech <maincontractor@gmail.com>
2023-05-31 22:31:50 +0100

    Created starter fragment classes for:IntroductionFragment, AccountOptionsFragment, LoginFragment, RegisterFragment

---
+++ app/src/main/java/com/melatech/ecommerce2b/ecommerce2b/ui/fragments/RegisterFragment.kt
@@ -0,0 +1,25 @@
+package com.melatech.ecommerce2b.ecommerce2b.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.melatech.ecommerce2b.R
+
+
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    companion object {
+
+        fun newInstance() = RegisterFragment()
+    }
+}
