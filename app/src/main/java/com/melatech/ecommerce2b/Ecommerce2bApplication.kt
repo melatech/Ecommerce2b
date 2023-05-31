91b6073c59acee84de85c12ce8ff3d1d3044d763
melatech <maincontractor@gmail.com>
2023-05-31 13:11:51 +0100

    Initial commit created folder structure

---
+++ app/src/main/java/com/melatech/ecommerce2b/Ecommerce2bApplication.kt
@@ -0,0 +1,4 @@
+package com.melatech.ecommerce2b
+
+class Ecommerce2bApplication {
+}
---
+++ app/src/main/java/com/melatech/ecommerce2b/ecommerce2b/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.melatech.ecommerce2b.ecommerce2b.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.melatech.ecommerce2b.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
