f59d6da4f35d175e5fa0ddbcd7a74871b60d3e57
melatech <maincontractor@gmail.com>
2023-05-31 19:14:46 +0100

    Added dimen folder and jpeg and drawable resources

---
+++ app/src/main/java/com/melatech/ecommerce2b/ecommerce2b/ui/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.melatech.ecommerce2b.ecommerce2b.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.melatech.ecommerce2b.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
--- app/src/main/java/com/melatech/ecommerce2b/ecommerce2b/ui/activities/ShoppingActivity.kt
+++ app/src/main/java/com/melatech/ecommerce2b/ecommerce2b/ui/activities/ShoppingActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.melatech.ecommerce2b.R
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
