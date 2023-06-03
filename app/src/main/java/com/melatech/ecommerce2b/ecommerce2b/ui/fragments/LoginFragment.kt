d0c4cd0a1e39229bfa49c8044597f19a93f2ce46
melatech <maincontractor@gmail.com>
2023-06-03 13:22:29 +0100

    Created reset password functionality with fragment extension function

--- app/src/main/java/com/melatech/ecommerce2b/ecommerce2b/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/melatech/ecommerce2b/ecommerce2b/ui/fragments/LoginFragment.kt
@@ -10,10 +10,12 @@ import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.melatech.ecommerce2b.R
 import com.melatech.ecommerce2b.databinding.FragmentLoginBinding
 import com.melatech.ecommerce2b.databinding.FragmentRegisterBinding
 import com.melatech.ecommerce2b.ecommerce2b.ui.activities.ShoppingActivity
+import com.melatech.ecommerce2b.ecommerce2b.ui.dialogs.setUpBottomSheetDialog
 import com.melatech.ecommerce2b.ecommerce2b.ui.viewmodels.LoginViewModel
 import com.melatech.ecommerce2b.ecommerce2b.ui.viewmodels.RegisterViewModel
 import com.melatech.ecommerce2b.util.Resource
@@ -46,7 +48,42 @@ class LoginFragment : Fragment() {
             buttonLoginLogin.setOnClickListener {
                 val email = edEmailLogin.text.toString().trim()
                 val password = edPasswordLogin.text.toString()
+                println("jason: buttonLoginLogin.setOnClickListener viewModel.login(email, password)")
                 viewModel.login(email, password)
+            }
+        }
+
+        binding.tvForgotPasswordLogin.setOnClickListener {
+            setUpBottomSheetDialog { email ->
+                println("jason: tvForgotPasswordLogin.setOnClickListener viewModel.resetPassword(email) ")
+                viewModel.resetPassword(email)
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            launch {
+                println("jason: viewModel.resetPassword.collect ")
+                viewModel.resetPassword.collect {
+                    when (it) {
+                        is Resource.Loading -> {}
+                        is Resource.Success -> {
+                            Snackbar.make(
+                                requireView(),
+                                "Reset link was sent to your email",
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+
+                        is Resource.Error -> {
+                            Snackbar.make(
+                                requireView(),
+                                "Error: ${it.message}",
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                        else -> Unit
+                    }
+                }
             }
         }
 
@@ -57,16 +94,20 @@ class LoginFragment : Fragment() {
                         is Resource.Loading -> {
                             binding.buttonLoginLogin.startAnimation()
                         }
+
                         is Resource.Success -> {
+                            binding.buttonLoginLogin.revertAnimation()
                             Intent(requireActivity(), ShoppingActivity::class.java).also { intent ->
                                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
                                 startActivity(intent)
                             }
                         }
+
                         is Resource.Error -> {
                             Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
                             binding.buttonLoginLogin.revertAnimation()
                         }
+
                         else -> Unit
                     }
                 }
--- app/src/main/java/com/melatech/ecommerce2b/ecommerce2b/ui/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/melatech/ecommerce2b/ecommerce2b/ui/viewmodels/LoginViewModel.kt
@@ -21,12 +21,14 @@ class LoginViewModel @Inject constructor(
     private val _login = MutableSharedFlow<Resource<FirebaseUser>>()
     val login = _login.asSharedFlow()
 
-    fun login(email: String, password: String) {
+    private val _resetPassword = MutableSharedFlow<Resource<String>>()
+    val resetPassword = _resetPassword.asSharedFlow()
 
-        viewModelScope.launch(Dispatchers.IO) { _login.emit(Resource.Loading()) }
+    fun login(email: String, password: String) {
+        viewModelScope.launch(IO) { _login.emit(Resource.Loading()) }
         firebaseAuth.signInWithEmailAndPassword(email, password)
             .addOnSuccessListener {
-                viewModelScope.launch((Dispatchers.IO)) {
+                viewModelScope.launch(IO) {
                     it.user?.let {
                         _login.emit(Resource.Success(it))
                     }
@@ -35,6 +37,25 @@ class LoginViewModel @Inject constructor(
             .addOnFailureListener {
                 viewModelScope.launch(IO) {
                     _login.emit(Resource.Error(it.message.toString()))
+                }
+            }
+    }
+
+    fun resetPassword(email: String) {
+        viewModelScope.launch(IO) {
+            _resetPassword.emit(Resource.Loading())
+        }
+
+        firebaseAuth
+            .sendPasswordResetEmail(email)
+            .addOnSuccessListener {
+                viewModelScope.launch(IO) {
+                    _resetPassword.emit(Resource.Success(email))
+                }
+            }
+            .addOnFailureListener {
+                viewModelScope.launch(IO) {
+                    _resetPassword.emit(Resource.Error(it.message.toString()))
                 }
             }
     }
