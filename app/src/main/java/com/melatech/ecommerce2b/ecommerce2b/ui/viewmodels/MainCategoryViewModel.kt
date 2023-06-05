5d5dc218569ed9e3e79cf9b328414a1c6d8327e2
melatech <maincontractor@gmail.com>
2023-06-05 13:27:07 +0100

    Created SpecialProductsAdapter for special products recycler view

---
+++ app/src/main/java/com/melatech/ecommerce2b/ecommerce2b/ui/viewmodels/MainCategoryViewModel.kt
@@ -0,0 +1,21 @@
+package com.melatech.ecommerce2b.ecommerce2b.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.melatech.ecommerce2b.data.Product
+import com.melatech.ecommerce2b.data.User
+import com.melatech.ecommerce2b.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+class MainCategoryViewModel @Inject constructor(
+    private val firestore: FirebaseFirestore
+): ViewModel() {
+
+    private val _specialProducts = MutableStateFlow<Resource<List<Product>>>(Resource.Unspecified())
+    val specialProducts: StateFlow<Resource<List<Product>>> = _specialProducts
+
+
+}
